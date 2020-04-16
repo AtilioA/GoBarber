9134279ac44dd57d9504e952c830c8e09f62f9f8
AtilioA <atiliodadalto@hotmail.com>
2020-04-16 13:11:12 -0300

    Add User model and migration

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,16 @@
 import { Router } from 'express';
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: "Hello, World!" });
+routes.get('/', async (req, res) => {
+  const user = await User.create({
+    name: 'Atílio Antônio',
+    email: 'atiliodadalto@guerrillamail.com',
+    hashed_password: 'p1l4ntr4',
+  });
+
+  return res.json(user);
 });
 
 module.exports = routes;
