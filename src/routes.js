f7b802357710d8fd0eedfe7b4a81a10de9f74663
AtilioA <atiliodadalto@hotmail.com>
2020-04-30 16:04:33 -0300

    Add multer to project

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,7 @@
 import { Router } from 'express';
 import User from './app/models/User';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import UserController from './app/models/controllers/UserController';
 import SessionController from './app/models/controllers/SessionController';
@@ -7,10 +9,16 @@ import SessionController from './app/models/controllers/SessionController';
 import authMiddleWare from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
+
+routes.post('/files', upload.single('file'), (req, res) => {
+  return res.json({ ok: true });
+});
 routes.use(authMiddleWare);
+
 routes.put('/users', UserController.update);
 
 module.exports = routes;
