ec0939ddc2cef5053b728380e685960616b42d62
AtilioA <atiliodadalto@hotmail.com>
2020-05-01 16:53:39 -0300

    Create ScheduleController and its listing route
    Return provider field on SessionController.store

--- src/routes.js
+++ src/routes.js
@@ -9,6 +9,7 @@ import authMiddleWare from './app/middlewares/auth';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -23,6 +24,8 @@ routes.post('/files', upload.single('file'), FileController.store);
 
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
+
+routes.get('/schedules', ScheduleController.index);
 
 routes.put('/users', UserController.update);
 
