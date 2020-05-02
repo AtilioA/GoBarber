fafb19f60074d692dfd0d1ebcaf0ff732be1ebb1
AtilioA <atiliodadalto@hotmail.com>
2020-05-02 11:58:07 -0300

    Create Notification model and migration

--- src/database/index.js
+++ src/database/index.js
@@ -3,10 +3,11 @@ import Sequelize from 'sequelize';
 import User from '../app/models/User';
 import File from '../app/models/File';
 import Appointment from '../app/models/Appointment';
+import Notification from '../app/models/Notification';
 
 import databaseConfig from '../config/database';
 
-const models = [User, File, Appointment];
+const models = [User, File, Appointment, Notification];
 
 class Database {
   constructor() {
