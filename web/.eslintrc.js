09b00a1371f026b8fc71e2270d622887dc64095e
AtilioA <atiliodadalto@hotmail.com>
2020-07-23 15:43:14 -0300

    Fix ESLint

--- web/.eslintrc.js
+++ web/.eslintrc.js
@@ -1,43 +1,45 @@
 module.exports = {
   env: {
+    browser: true,
     es6: true,
-    jest: true,
-    browser: true
   },
-  extends: ["airbnb", "prettier", "prettier/react"],
+  extends: [
+    'eslint:recommended',
+    'react-app',
+    'prettier',
+    'prettier/react', // Rules for React
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended', // Rules for React Hooks
+  ],
+  plugins: ['prettier'],
+  // plugins: ["react", "jsx-a11y", "import", "react-hooks", "prettier"],
   globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly",
-    __DEV__: true
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
   },
   parserOptions: {
     ecmaFeatures: {
-      jsx: true
+      jsx: true,
     },
-    ecmaVersion: 2018,
-    sourceType: "module"
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    parser: 'babel-eslint',
   },
-  plugins: ["react", "jsx-a11y", "import", "react-hooks", "prettier"],
   rules: {
-    "prettier/prettier": "error",
-    "react/jsx-filename-extension": ["error", { extensions: [".js", ".jsx"] }],
-    "import/prefer-default-export": "off",
-    "no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
-    "react/jsx-one-expression-per-line": "off",
-    "global-require": "off",
-    "react-native/no-raw-text": "off",
-    "no-param-reassign": "off",
-    "no-underscore-dangle": "off",
-    camelcase: "off",
-    "no-console": ["error", { allow: ["tron"] }],
-    "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "warn"
-  },
-  settings: {
-    "import/resolver": {
-      "babel-plugin-root-import": {
-        rootPathSuffix: "src"
-      },
-    },
+    'prettier/prettier': 'error',
+    semi: ['error', 'always'], // Enforce semicolons
+    camelcase: 'off',
+    'class-methods-use-this': 'off',
+    'no-param-reassign': 'off',
+    'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
+    'react/jsx-one-expression-per-line': 'off',
+    'global-require': 'off',
+    'react-native/no-raw-text': 'off',
+    'no-underscore-dangle': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.js'] }],
+    'import/prefer-default-export': 'off',
+    'no-console': ['warn', { allow: ['tron'] }],
   },
 };
