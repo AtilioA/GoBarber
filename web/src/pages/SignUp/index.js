e24925dbe698e08d27846e8bc2f2ff98b0e0e434
AtilioA <atiliodadalto@hotmail.com>
2020-07-30 00:11:41 -0300

    Create sign up functionality

--- web/src/pages/SignUp/index.js
+++ web/src/pages/SignUp/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
 import * as Yup from 'yup';
 
 import logo from '../../assets/logo.svg';
+
+import { signUpRequest } from '../../store/modules/auth/actions';
 
 const schema = Yup.object().shape({
   name: Yup.string().required('Name is required'),
@@ -12,6 +15,12 @@ const schema = Yup.object().shape({
 })
 
 function SignUp() {
+  const dispatch = useDispatch();
+
+  function handleSubmit({ name, email, password }) {
+    dispatch(signUpRequest(name, email, password));
+  }
+
   return (
     <>
       <img src={logo} alt="GoBarber" />
