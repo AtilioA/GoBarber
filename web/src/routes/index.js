e9d66b6afe3240e1de5766c17797d50fe66c88f4
AtilioA <atiliodadalto@hotmail.com>
2020-08-06 23:32:56 -0300

    Fix routes/index.js
    Remove unnecessary BrowserRouter

--- web/src/routes/index.js
+++ web/src/routes/index.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import { Switch, BrowserRouter } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 import Route from './Route';
 
 import SignIn from '../pages/SignIn';
 import SignUp from '../pages/SignUp';
-
 import Dashboard from '../pages/Dashboard';
 import Profile from '../pages/Profile';
 
 export default function Routes() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/" exact component={SignIn} />
-        <Route path="/register" exact component={SignUp} />
-        <Route path="/dashboard" isPrivate component={Dashboard} />
-        <Route path="/profile" isPrivate component={Profile} />
+    <Switch>
+      <Route path="/" exact component={SignIn} />
+      <Route path="/register" component={SignUp} />
 
-        {/* <Route path="/" component={() => <h1>404</h1>} /> */}
-      </Switch>
-    </BrowserRouter>
+      <Route path="/dashboard" component={Dashboard} isPrivate />
+      <Route path="/profile" component={Profile} isPrivate />
+    </Switch>
   );
 }
