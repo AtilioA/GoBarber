85cfdc661a02ebc4d83e0d4bde9129389b1d4c22
AtilioA <atiliodadalto@hotmail.com>
2020-08-06 21:45:57 -0300

    Fix Header profile avatar

--- web/src/components/Header/index.js
+++ web/src/components/Header/index.js
@@ -1,37 +1,41 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
 
-import Notifications from '../Notifications';
+import Notifications from '~/components/Notifications';
 
-import logo from '../../assets/logo_purple.svg';
+import logo from '~/assets/logo_purple.svg';
 
 import { Container, Content, Profile } from './styles';
 
-function Header() {
-  const profile = useSelector(state => state.user.profile);
+export default function Header() {
+   const profile = useSelector(state => state.user.profile);
 
-  return (
-  <Container>
-    <Content>
-      <nav>
-        <img src={logo} alt="GoBarber" />
-        <Link to="/dashboard">Dashboard</Link>
-      </nav>
+   return (
+      <Container>
+         <Content>
+            <nav>
+               <img src={logo} alt="GoBarber" />
+               <Link to="/dashboard">Dashboard</Link>
+            </nav>
 
-      <aside>
-        <Notifications />
-        <Profile>
-          <div>
-            <strong>{profile.name}</strong>
-            <Link to="/profile">My profile</Link>
-          </div>
-          <img src={profile.avatar_url} alt="Your avatar" />
-        </Profile>
-      </aside>
-    </Content>
-  </Container>
-  );
+            <aside>
+               <Notifications />
+
+               <Profile>
+                  <div>
+                     <strong>{profile.name}</strong>
+                     <Link to="/profile">My profile</Link>
+                  </div>
+                  <img
+                     src={
+                        profile.avatar && profile.avatar.url
+                     }
+                     alt={profile.name}
+                  />
+               </Profile>
+            </aside>
+         </Content>
+      </Container>
+   );
 }
-
-export default Header;
